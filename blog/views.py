1ec35b04b927be1046a0e441b62ffb32dfc56309
Fernando <ferdc02@gmail.com>
2019-03-01 18:07:45 -0800

    End of Ch5, Adding Post model.

--- blog/views.py
+++ blog/views.py
@@ -2,26 +2,6 @@ from django.shortcuts import render
 from .models import Post
 
 
-posts = [
-    {
-        'author': 'Feron',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_psted': 'Agosto 23, 2018'
-    },
-    {
-        'author': 'Troy',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_psted': 'Agosto 29, 2018'
-
-
-
-    
-    }
-
-]
-
 def home(request):
     context = {
         'posts': Post.objects.all()
